166dce6803ac54be5f0460e98842a9f198562423
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2024-04-09 12:49:12 +0000

    Update index.mjs
    Post request

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express()
+app.use(express.json())
 const PORT = process.env.PORT || 3000
 
 
@@ -46,6 +47,13 @@ app.get("/api/users", (req, res) => {
 
     return res.send(mockUsers);
 });
+
+app.post("/api/users", (req, res) => {
+    const {body } = req
+    const newUser = {id: mockUsers[mockUsers.length - 1].id + 1, ...body}
+    mockUsers.push(newUser)
+    return res.status(201).send(newUser);
+})
 
 
 
