0ac6f1ed883aae22a1cd83ba247cb5ed4696de65
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2024-04-17 14:00:25 +0000

    product validation schema

---
+++ src/utils/productsValidationSchema.mjs
@@ -0,0 +1,33 @@
+export const createValidationSchema = {
+    productname:{
+        isLength:{
+            options:{
+                min:3,
+                max:32,
+            },
+            errorMessage: 'username must be 3-32 characters'
+        },
+        notEmpty: {
+            errorMessage: "product name cannot be empty"
+        },
+        isString:{
+            errorMessage: 'product name must be a string'
+        }
+    },
+
+    price:{
+        isLength:{
+            options:{
+                min:1,
+                max:15,
+            },
+            errorMessage: 'price must be 1 - 15 characters'
+        },
+        notEmpty: {
+            errorMessage: 'price must not be empty'
+        },
+        isNumeric: {
+            errorMessage:'price must be a integer'
+        }
+    },
+};
