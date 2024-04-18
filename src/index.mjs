9f6838edcc3010012fb7ac6d89d3c69221a163e8
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2024-04-18 11:32:58 +0000

    organise routes

--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,10 @@
 import express from "express";
-import userRouter from "./routes/users.mjs"
-import productRouter from "./routes/products.mjs"
-import defaultRouter from "./routes/default.mjs"
+import routes from "./routes/index.mjs";
 
 
 const app = express()
-
 app.use(express.json())
-app.use(defaultRouter)
-app.use(userRouter)
-app.use(productRouter)
+app.use(routes)
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
