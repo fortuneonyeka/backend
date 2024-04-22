da623ffebd8de67ff4b9d6472a57684d382a2030
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2024-04-22 14:14:10 +0000

    add password params

--- src/utils/constants.mjs
+++ src/utils/constants.mjs
@@ -1,8 +1,8 @@
 export const mockUsers = [
-    { username: "fortune", pod: "developer", id: 1 },
-    { username: "val", pod: "app service", id: 2 },
-    { username: "bose", pod: "developer", id: 3 },
-    { username: "mathew", pod: "developer", id: 4 },
+    { username: "fortune", pod: "developer", id: 1, password: "12345"},
+    { username: "val", pod: "app service", id: 2, password: "123456" },
+    { username: "bose", pod: "developer", id: 3, password: "1234567" },
+    { username: "mathew", pod: "developer", id: 4, password: "12345678" },
 ]
 
 
