2fac1ad9b098fe6fdccff61f6910ab1ed0e6c1c0
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2024-04-22 14:14:43 +0000

    user auth

--- src/index.mjs
+++ src/index.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import routes from "./routes/index.mjs";
 import cookieParser from "cookie-parser";
 import session from "express-session";
+import { mockUsers } from "./utils/constants.mjs";
 
 const app = express()
 app.use(express.json())
@@ -9,7 +10,7 @@ app.use(cookieParser("helloworld"))
 app.use(session({
     secret: "anson the dev",
     saveUninitialized: false,
-    resave:false,
+    resave: false,
     cookie: {
         maxAge: 60000 * 60
     }
@@ -21,5 +22,21 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
 
+app.post("/api/auth", (req, res) => {
+    const { body: { username, password } } = req
+
+    const findUser = mockUsers.find((user) => user.username === username)
+
+    if (!findUser || findUser.password !== password) return res.status(401).send({ message: "Bad Credentioanls" })
+
+    req.session.user = findUser;
+    return res.status(200).send(findUser)
+
+})
+
+app.get("/api/auth/status", (req, res) => {
+    req.sessionStore.get(req.sessionID, (err, session) => { console.log(session);})
+    return req.session.user ? res.status(200).send(req.session.user) : res.status(401).send({ message: "Not Authenticated" })
+})
 
 
